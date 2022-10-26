070a91371083c2073b1d238fc3df0612c3298792
Omer Oreg <sarhatabaot1@gmail.com>
2022-10-26 12:33:47 +0300

    Updated papi to latest

--- src/main/java/me/itsmas/forgemodblocker/placeholder/Placeholders.java
+++ src/main/java/me/itsmas/forgemodblocker/placeholder/Placeholders.java
@@ -1,30 +1,46 @@
 package me.itsmas.forgemodblocker.placeholder;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.PlaceholderHook;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.clip.placeholderapi.external.EZPlaceholderHook;
 import me.itsmas.forgemodblocker.ForgeModBlocker;
 import me.itsmas.forgemodblocker.mods.ModData;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Handles {@link PlaceholderAPI} hooks
  */
-public class Placeholders extends EZPlaceholderHook {
+public class Placeholders extends PlaceholderExpansion {
     /**
      * The plugin instance
      */
     private final ForgeModBlocker plugin;
 
     public Placeholders(ForgeModBlocker plugin) {
-        super(plugin, "forgemodblocker");
-
         this.plugin = plugin;
+        this.register();
+    }
 
-        hook();
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "forgemodblocker";
     }
 
     @Override
-    public String onPlaceholderRequest(Player player, String identifier) {
+    public @NotNull String getAuthor() {
+        return "sarhatabaot";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, @NotNull String identifier) {
         if (player == null) {
             return null;
         }

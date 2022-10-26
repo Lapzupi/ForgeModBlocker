6ce001dce59e34b6ca66b962dc541d148818c456
Omer Oreg <sarhatabaot1@gmail.com>
2022-10-26 12:29:12 +0300

    Update ModsCommand.java

--- src/main/java/me/itsmas/forgemodblocker/command/ModsCommand.java
+++ src/main/java/me/itsmas/forgemodblocker/command/ModsCommand.java
@@ -8,6 +8,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 
@@ -19,7 +20,7 @@ public class ModsCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender,@NotNull  Command cmd,@NotNull  String label, String[] args) {
         if (!Permission.hasPermission(sender, Permission.MODS_COMMAND)) {
             Message.send(sender, Message.NO_PERMISSION);
             return true;
